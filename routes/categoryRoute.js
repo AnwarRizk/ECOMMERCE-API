7ce330d4c2857cc037b8e0f7f9bc2ba906045bb2
AnwarRizk <anwar01285593087@gmail.com>
2026-03-07 19:11:33 +0200

    `Added express-validator package and updated dependencies`

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const {
+  getCategoryValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidator");
+
 const router = express.Router();
 
 const {
@@ -9,11 +16,14 @@ const {
   deleteCategory,
 } = require("../services/categoryService");
 
-router.route("/").get(getCategories).post(createCategory);
+router
+  .route("/")
+  .get(getCategories)
+  .post(createCategoryValidator, createCategory);
 router
   .route("/:id")
-  .get(getCategory)
-  .put(updateCategory)
-  .delete(deleteCategory);
+  .get(getCategoryValidator, getCategory)
+  .put(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
 
 module.exports = router;
