c352bb46c334237b639208cd35f24d1277452ca6
AnwarRizk <anwar01285593087@gmail.com>
2026-03-31 19:31:17 +0200

    Apply upload image middleware to brand route and service

--- services/brandService.js
+++ services/brandService.js
@@ -1,6 +1,28 @@
-/* eslint-disable new-cap */
+const asyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
+const sharp = require("sharp");
+
 const Brand = require("../models/brandModel");
 const factory = require("./handlersFactory");
+const { uploadSingleImage } = require("../middlewares/uploadImageMiddleware");
+
+exports.uploadBrandImage = uploadSingleImage("image");
+
+exports.resizeImage = asyncHandler(async (req, res, next) => {
+  if (!req.file) return next();
+
+  const filename = `brand-${uuidv4()}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(600, 600)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`uploads/brands/${filename}`);
+
+  req.body.image = filename;
+
+  next();
+});
 
 // @desc    Get all brands
 // @route   GET /api/v1/brands
